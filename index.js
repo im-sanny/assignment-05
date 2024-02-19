784dd340ca19f2bf0c0a5e90372655500c8bea30
im-sanny <raselparvezsanny@gmail.com>
2024-02-19 12:06:09 +0600

    primary setup for js is done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('allah vorosha');
+const allSmBtn = document.getElementsByClassName('add-btn');
+
+let count = 40;
+for (const btn of allSmBtn) {
+    btn.addEventListener('click', function (e) {
+        count -= 1;
+        setInnerText('fortySeats', count);
+
+
+        const sitNumber = setInnerText('purchase-r-one', e.target.innerText)
+        console.log(e.target.innerText)
+    })
+}
+
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+}
+
